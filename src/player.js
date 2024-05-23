dc2f584343d44840484f013049e7ab8ecf8de80a
Christopher Solanilla <chrissolanilla@gmail.com>
2024-05-23 14:16:19 -0400

    adds style to button where it greys out if invalid selections

--- src/player.js
+++ src/player.js
@@ -76,21 +76,39 @@ function selectWord(word, wordElement, checkbox) {
 	console.log('Selected Words:', selectedWords);
 	updateSelectionStyles();
 }
+//this reduces a lot of redundant code in update seleciton styles
+function updateButtonStyles(buttonId, isEnabled) {
+    const button = document.getElementById(buttonId);
+    if (isEnabled) {
+        button.classList.remove('greyOutButton');
+        button.classList.add('styled-button');
+    } else {
+        button.classList.remove('styled-button');
+        button.classList.add('greyOutButton');
+    }
+}
 
 function updateSelectionStyles() {
-	const wordsGrid = document.querySelector('.wordsPreview');
-	wordsGrid.querySelectorAll('.previewItem').forEach(item => {
-		item.classList.remove('selected-4', 'selected-8', 'selected-12', 'selected-16');
-	});
-	selectedWords.forEach((word, index) => {
-		const checkbox = [...document.querySelectorAll('.previewItem input[type="checkbox"]')]
-			.find(input => input.nextElementSibling.textContent === word);
-		const item = checkbox.parentNode;
-		if (index < 4) item.classList.add('selected-4');
-		else if (index < 8) item.classList.add('selected-8');
-		else if (index < 12) item.classList.add('selected-12');
-		else item.classList.add('selected-16');
-	});
+    const wordsGrid = document.querySelector('.wordsPreview');
+    wordsGrid.querySelectorAll('.previewItem').forEach(item => {
+        item.classList.remove('selected-4', 'selected-8', 'selected-12', 'selected-16');
+    });
+
+    selectedWords.forEach((word, index) => {
+        const checkbox = [...document.querySelectorAll('.previewItem input[type="checkbox"]')]
+            .find(input => input.nextElementSibling.textContent === word);
+        const item = checkbox.parentNode;
+        if (index < 4) item.classList.add('selected-4');
+        else if (index < 8) item.classList.add('selected-8');
+        else if (index < 12) item.classList.add('selected-12');
+        else item.classList.add('selected-16');
+    });
+
+    const selectionCount = selectedWords.length;
+    updateButtonStyles('check4', selectionCount === 4);
+    updateButtonStyles('check8', selectionCount === 8);
+    updateButtonStyles('check12', selectionCount === 12);
+    updateButtonStyles('check16', selectionCount === 16);
 }
 
 function checkSelection(count) {
@@ -111,6 +129,10 @@ function checkSelection(count) {
         alert('All selected groups are correct!');
     } else {
         alert('Incorrect. Try again.');
+		document.getElementById('check4').classList.remove('styled-button');
+		document.getElementById('check8').classList.remove('styled-button');
+		document.getElementById('check12').classList.remove('styled-button');
+		document.getElementById('check16').classList.remove('styled-button');	
     }
     selectedWords = [];
     document.querySelectorAll('.previewItem input[type="checkbox"]:checked').forEach(checkbox => {
